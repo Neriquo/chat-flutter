5b5d961d42200e1f083680c8d98e7262a4f84b8a
henrique <henrique@dootix.com>
2025-01-17 16:13:34 +0100

    Upgrade flutter

--- android/app/src/main/java/co/appdootix/flash_chat/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package co.appdootix.flash_chat;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-
-}
---
+++ android/app/src/main/kotlin/com/example/test_import/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.dootix.chat_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
